31ef5910f3a5db2542c51671c7a5cc38d97aeecd
dtgreene <dylan.greene@shipt.com>
2023-09-10 22:31:44 -0500

    Update link

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -54,6 +54,7 @@ export const App = () => {
         <div className="text-2xl">YAN Nutritent Calculator 2.1</div>
         <a
           className={cx(styles.link, 'text-sm')}
+          target="_blank"
           href="https://docs.google.com/spreadsheets/d/1W8Pp52vFx9g-Uk7aq4WK66Kg_TI5nTrI32sBc5fGaPU/edit#gid=0"
         >
           Original Google sheet
